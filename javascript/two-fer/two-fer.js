8c7cf7fe5310d8c4d19865d6ce9f2a73965d4a53
Stephen Ball <sdball@gmail.com>
2017-12-18 17:04:01 -0500

    Hello Exercism!

---
+++ javascript/two-fer/two-fer.js
@@ -0,0 +1,8 @@
+var TwoFer = function () {};
+
+TwoFer.prototype.twoFer = function (who) {
+  if (who) { return `One for ${who}, one for me.`; }
+  else     { return 'One for you, one for me.'; }
+};
+
+module.exports = TwoFer;
