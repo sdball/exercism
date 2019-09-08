71e430d83bb2e4a02b14e9e0716677b3081bce13
Stephen Ball <sdball@gmail.com>
2019-09-07 20:23:37 -0400

    Rust: raindrops solution
    Fun with currying and closures!

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,3 +1,32 @@
+// fancy!
+fn divisible_by(n: u32) -> impl Fn(u32) -> bool {
+    // ooh closure
+    move |x| x % n == 0
+}
+
 pub fn raindrops(n: u32) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+    let mut messages = Vec::new();
+
+    // just trying out some currying in Rust ^_^
+    let divisible_by_3 = divisible_by(3);
+    let divisible_by_5 = divisible_by(5);
+    let divisible_by_7 = divisible_by(7);
+
+    if divisible_by_3(n) {
+        messages.push(String::from("Pling"));
+    }
+
+    if divisible_by_5(n) {
+        messages.push(String::from("Plang"));
+    }
+
+    if divisible_by_7(n) {
+        messages.push(String::from("Plong"));
+    }
+
+    if messages.is_empty() {
+        messages.push(n.to_string());
+    }
+
+    return messages.join("");
 }
