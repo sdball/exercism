b9efa05c839a4ca330aa1b30f84ec902d0cf2da8
Stephen Ball <sdball@gmail.com>
2019-09-11 20:10:17 -0400

    Rust: nth prime (hacky hack solution)

---
+++ rust/nth-prime/src/lib.rs
@@ -0,0 +1,21 @@
+fn is_prime(n: u32, primes: Vec<u32>) -> bool {
+    for p in primes.iter() {
+        if n % p == 0 {
+            return false;
+        }
+    }
+    true
+}
+
+pub fn nth(n: u32) -> u32 {
+    let mut primes = vec![2];
+    for i in 3..std::u32::MAX {
+        if is_prime(i, primes.clone()) {
+            primes.push(i);
+        }
+        if primes.len() as u32 > n {
+            break;
+        }
+    }
+    *primes.iter().nth(n as usize).unwrap()
+}
