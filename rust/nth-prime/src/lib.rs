2cd9a5cf7778933aa74649d8a3a7809ffb4d5f39
Stephen Ball <sdball@gmail.com>
2019-09-13 21:26:19 -0400

    Pass primes by reference instead of clone
    Woo! No more silly cloning the vector so the is_prime function won't
    take ownership of primes from the nth function.

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,4 +1,4 @@
-fn is_prime(n: u32, primes: Vec<u32>) -> bool {
+fn is_prime(n: u32, primes: &[u32]) -> bool {
     for p in primes.iter() {
         if n % p == 0 {
             return false;
@@ -10,7 +10,7 @@ fn is_prime(n: u32, primes: Vec<u32>) -> bool {
 pub fn nth(n: u32) -> u32 {
     let mut primes = vec![2];
     for i in 3..std::u32::MAX {
-        if is_prime(i, primes.clone()) {
+        if is_prime(i, &primes) {
             primes.push(i);
         }
         if primes.len() as u32 > n {
